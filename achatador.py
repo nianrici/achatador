40152c0f25df48ec78459a52a8d2d7cef94d7899
Nicolas Riquelme <nicolas@riquelmes.com>
2023-07-11 23:03:34 +0200

    Optimización de código

--- achatador.py
+++ achatador.py
@@ -1,16 +1,39 @@
-import os
 import subprocess
+import glob
+import platform
 
-achata = "mogrify -geometry 1:1 *.bmp"
-recorta = "convert *.bmp -crop x800+0+320 +repage HelpScreens.png"
+INPUT_DIR = "."
+OUTPUT_FILE = "HelpScreens.png"
+IMAGE_PATTERN = "*.bmp"
 
-dirname = "."
-deleter = os.listdir(dirname)
 
-subprocess.call(achata, shell=True)
-subprocess.call(recorta, shell=True)
-# os.remove("*.bmp")
+def process_images_windows():
+    for command in [
+        f"mogrify -geometry 1:1 {IMAGE_PATTERN}",
+        f"convert {IMAGE_PATTERN} -crop x800+0+320 +repage {OUTPUT_FILE}"
+    ]:
+        subprocess.call(command, shell=True)
 
-for i in deleter:
-    if i.endswith(".bmp"):
-        os.remove(i)
+    for file in glob.glob(f"{INPUT_DIR}\\{IMAGE_PATTERN}"):
+        subprocess.call(["del", file])
+
+
+def process_images_linux():
+    for command in [
+        f"mogrify -geometry 1:1 {IMAGE_PATTERN}",
+        f"convert {IMAGE_PATTERN} -crop x800+0+320 +repage {OUTPUT_FILE}"
+    ]:
+        subprocess.call(command, shell=True)
+
+    for file in glob.glob(f"{INPUT_DIR}/{IMAGE_PATTERN}"):
+        subprocess.call(["rm", file])
+
+
+system = platform.system()
+
+if system == "Windows":
+    process_images_windows()
+elif system == "Linux":
+    process_images_linux()
+else:
+    print("Este script solo es compatible con Windows y Linux.")
